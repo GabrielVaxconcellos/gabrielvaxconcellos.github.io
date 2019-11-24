5deeb6520c0128c4d34a1f3488b1b06774cc4c85
Gabriel Vasconcellos <gabrielvasconcellosilva@hotmail.com>
2019-11-24 15:06:02 -0300

    Selecao da listagem

---
+++ js/index.js
@@ -0,0 +1,56 @@
+// document.querySelector('.segura-lista-gerada').addEventListener('click',
+// function(){
+//     document.querySelector('.segura-lista-gerada').classList.toggle('segura-lista-gerada-verde')
+// })
+
+document.querySelector('.segura-lista-gerada1').addEventListener('click',
+function(){
+    document.querySelector('.segura-lista-gerada1').classList.toggle('segura-lista-gerada-verde')
+})
+
+document.querySelector('.segura-lista-gerada2').addEventListener('click',
+function(){
+    document.querySelector('.segura-lista-gerada2').classList.toggle('segura-lista-gerada-verde')
+})
+
+document.querySelector('.segura-lista-gerada3').addEventListener('click',
+function(){
+    document.querySelector('.segura-lista-gerada3').classList.toggle('segura-lista-gerada-verde')
+})
+
+document.querySelector('.segura-lista-gerada4').addEventListener('click',
+function(){
+    document.querySelector('.segura-lista-gerada4').classList.toggle('segura-lista-gerada-verde')
+})
+
+document.querySelector('.segura-lista-gerada5').addEventListener('click',
+function(){
+    document.querySelector('.segura-lista-gerada5').classList.toggle('segura-lista-gerada-verde')
+})
+
+document.querySelector('.segura-lista-gerada6').addEventListener('click',
+function(){
+    document.querySelector('.segura-lista-gerada6').classList.toggle('segura-lista-gerada-verde')
+})
+
+document.querySelector('.segura-lista-gerada7').addEventListener('click',
+function(){
+    document.querySelector('.segura-lista-gerada7').classList.toggle('segura-lista-gerada-verde')
+})
+
+document.querySelector('.segura-lista-gerada8').addEventListener('click',
+function(){
+    document.querySelector('.segura-lista-gerada8').classList.toggle('segura-lista-gerada-verde')
+})
+
+document.querySelector('.segura-lista-gerada9').addEventListener('click',
+function(){
+    document.querySelector('.segura-lista-gerada9').classList.toggle('segura-lista-gerada-verde')
+})
+
+document.querySelector('.segura-lista-gerada10').addEventListener('click',
+function(){
+    document.querySelector('.segura-lista-gerada10').classList.toggle('segura-lista-gerada-verde')
+})
+
+
